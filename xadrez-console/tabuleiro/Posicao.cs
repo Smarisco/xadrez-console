b5516467348ff63bc84512f217576c532bdf1583
Stela Marisco <smarisco@yahoo.com.br>
2021-01-05 16:12:27 -0400

    Trabalho finalizado

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -1,23 +1,27 @@
-﻿using System;
-
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
+
         public int linha { get; set; }
         public int coluna { get; set; }
 
-        
         public Posicao(int linha, int coluna)
         {
             this.linha = linha;
             this.coluna = coluna;
         }
+
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
         public override string ToString()
         {
-            return linha 
-                + "," 
+            return linha
+                + ", "
                 + coluna;
         }
     }
